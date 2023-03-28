54875b3d4a22d73c2004e5f10b660d392294beb9
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-03-28 20:39:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -46,13 +46,13 @@ const readMore = more.repeat(4001)
 
 global.owner = [
 ['6285923463470', 'AnggaKz', true]
-['6285219347928', 'Pacar🥰', true]
+['6285219347928', 'Devi oktavia', true]
 ]
 global.mods = []
 global.prems = []
 global.nomorbot = '6283191486089'
 global.nomorown = '6285923463470'
-global.nomorown1 = '6285923463470'
+global.nomorown1 = '6285219347928'
 global.nomorown2 = '6285219347928'
 global.nomorwa = '0'
 
