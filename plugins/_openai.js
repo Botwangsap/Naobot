23f5d98ae04b04743549113866039f10bb907808
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-04-03 09:40:37 +0700

    Update _openai.js

--- plugins/_openai.js
+++ plugins/_openai.js
@@ -1,25 +1,32 @@
-import { Configuration, OpenAIApi } from "openai";
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `Contoh:\n${usedPrefix + command} Apa itu OpenAI`
-const configuration = new Configuration({
-    apiKey: "" //api key bisa didapatkan dari https://openai.com/api/
-});
-const openai = new OpenAIApi(configuration);
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: text,
-            temperature: 0,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0
-        });
-            m.reply(response.data.choices[0].text)
-    }
+import { Configuration, OpenAIApi } from "openai"
+var handler = async (m, { conn, usedPrefix, command, text }) => {
+try {
+            if (!text) return m.reply(`Chat dengan AI.\n\nContoh:\n${usedPrefix}${command} Halo? `);
+            const configuration = new Configuration({
+              apiKey: `${global.openai}`, // edit on config.js
+            });                  
+            const openai = new OpenAIApi(configuration);
+
+            const response = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [{role: "user", content: text}],
+          });
+          m.reply(`${response.data.choices[0].message.content}`);
+          } catch (error) {
+          if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+            console.log(`${error.response.status}\n\n${error.response.data}`);
+          } else {
+            console.log(error);
+            m.reply("Maaf, sepertinya ada yang error :"+ error.message);
+          }
+        }
+}
 handler.help = ['ai <pertanyaan>']
 handler.tags = ['ai']
 handler.command = /^(ai)$/i
-handler.limit = false
-handler.register = true
+handler.limit = true
+handler.register = false
 
 export default handler
