64e91a4580dcc6f181b53ecc5e5869c6a204adb4
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-04-05 12:18:35 +0700

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -3,8 +3,6 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
     let bcbg = 'https://telegra.ph/file/da19d1c4c1e23b8b9c72e.jpg'
     conn.m.reply(`${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│◦❒ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null,
-        mentions: users
-    })
 }
 
 handler.help = ['tagall']
