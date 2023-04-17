aadc9ce2bd0270c6e77c7db56261319d41060432
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-04-18 04:17:56 +0700

    Update _openai.js

--- plugins/_openai.js
+++ plugins/_openai.js
@@ -1,11 +1,12 @@
-import { Configuration, OpenAIApi } from "openai"
-var handler = async (m, { conn, usedPrefix, command, text }) => {
+import fetch from "node-fetch"
+import { generateWAMessageFromContent } from "@adiwajshing/baileys"
+import fs from 'fs'
+import { Configuration, OpenAIApi } from 'openai'
+const configuration = new Configuration({ organization: `${global.org}`, apiKey: `${global.openai}` });
+const openai = new OpenAIApi(configuration);
+let handler = async (m, { conn, text }) => {
 try {
             if (!text) return m.reply(`Chat dengan AI.\n\nContoh:\n${usedPrefix}${command} Halo? `);
-            const configuration = new Configuration({
-              apiKey: `${global.openai}`, // edit on config.js
-            });                  
-            const openai = new OpenAIApi(configuration);
 
             const response = await openai.createChatCompletion({
           model: "gpt-3.5-turbo",
@@ -26,7 +27,7 @@ try {
 handler.help = ['ai <pertanyaan>']
 handler.tags = ['ai']
 handler.command = /^(ai)$/i
-handler.limit = true
+handler.limit = false
 handler.register = false
 
 export default handler
