aeb74994c191bd5b9941b583726d5cb3a04bd072
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-05-05 14:34:56 +0700

    Update tool-readmore.js

--- plugins/tool-readmore.js
+++ plugins/tool-readmore.js
@@ -7,6 +7,7 @@ let handler = async (m, { conn, text }) => {
 handler.help = ['readmore', 'spoiler'].map(v => v + ' <teks>|<teks>')
 handler.tags = ['tools']
 handler.command = /^(spoiler|hidetext|readmore|selengkapnya)$/i
+handler.limit = true
 
 export default handler
 
