4a04599493c9a980256af49c56659171ae24a072
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-05-07 06:24:42 +0700

    Update game-tebaklagu_hint.js

--- plugins/game-tebaklagu_hint.js
+++ plugins/game-tebaklagu_hint.js
@@ -3,10 +3,12 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.tebaklagu)) throw false
     let json = conn.tebaklagu[id][1]
-    let nya = json.judul
-    let nyanya = nya.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_')
-    m.reply('```' + nyanya + '```')
+    conn.sendButton(m.chat, '```' + json.judul.replace(/[AIUEOaiueo]/ig, '_') + '```', author, null, [
+        ['Nyerah', 'menyerah']
+    ], m)
 }
-handler.command = /^cek$/i
+handler.command = /^hlag$/i
+
 handler.limit = true
+
 export default handler
