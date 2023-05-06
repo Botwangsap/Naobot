d026c999d59dfbb0cd425f5654af63c62ac5f968
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-05-06 23:40:53 +0700

    Update game-susunkata.js

--- plugins/game-susunkata.js
+++ plugins/game-susunkata.js
@@ -1,32 +1,31 @@
 import fetch from 'node-fetch'
-
 let timeout = 120000
-let poin = 500
-let handler = async (m, { conn, usedPrefix }) => {
+let poin = 4999
+let handler = async (m, { conn, command, usedPrefix }) => {
+let imgr = flaaa.getRandom()
+
     conn.susunkata = conn.susunkata ? conn.susunkata : {}
     let id = m.chat
     if (id in conn.susunkata) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.susunkata[id][0])
+        conn.sendButton(m.chat, 'Masih ada soal belum terjawab di chat ini', author, null, buttons, conn.susunkata[id][0])
         throw false
     }
-    let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/susunkata.json')
-    if (!res.ok) throw eror
-    let data = await res.json()
-    let json = data[Math.floor(Math.random() * data.length)]
-    let caption = `
-${json.soal}
-
-Tipe: ${json.tipe}
-
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/susunkata.json')).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+  let caption = `*${command.toUpperCase()}*
+  ${json.soal}
+  ${json.tipe}
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}suka untuk bantuan
+Ketik ${usedPrefix}hsus untuk bantuan
 Bonus: ${poin} XP
-`.trim()
+    `.trim()
     conn.susunkata[id] = [
-        await conn.sendBut(m.chat, caption, wm, 'Bantuan', '.suka'),
+        await conn.sendButton(m.chat, caption, author, `${imgr + command}`, buttons, m),
         json, poin,
-        setTimeout(async () => {
-            if (conn.susunkata[id]) await conn.sendBut(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, wm, 'Susun Kata', '.susunkata')
+        setTimeout(() => {
+            if (conn.susunkata[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, author, null, [
+                ['susunkata', '/susunkata']
+            ], conn.susunkata[id][0])
             delete conn.susunkata[id]
         }, timeout)
     ]
@@ -36,3 +35,8 @@ handler.tags = ['game']
 handler.command = /^susunkata/i
 
 export default handler
+
+const buttons = [
+    ['Hint', '/hsus'],
+    ['Nyerah', 'menyerah']
+]
