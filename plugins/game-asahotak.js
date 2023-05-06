9c5d2ab30b8d3fe05c83dac2d2c5a4987b390af8
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-05-07 06:05:16 +0700

    Update game-asahotak.js

--- plugins/game-asahotak.js
+++ plugins/game-asahotak.js
@@ -1,27 +1,30 @@
-
 import fetch from 'node-fetch'
 let timeout = 120000
-let poin = 2500
-let handler = async (m, { conn, usedPrefix }) => {
+let poin = 4999
+let handler = async (m, { conn, command, usedPrefix }) => {
+let imgr = flaaa.getRandom()
+
     conn.asahotak = conn.asahotak ? conn.asahotak : {}
     let id = m.chat
     if (id in conn.asahotak) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.asahotak[id][0])
+        conn.sendButton(m.chat, 'Masih ada soal belum terjawab di chat ini', author, null, buttons, conn.asahotak[id][0])
         throw false
     }
     let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/asahotak.json')).json()
-    let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `
-${json.soal}
+  let json = src[Math.floor(Math.random() * src.length)]
+  let caption = `*${command.toUpperCase()}*
+  ${json.soal}
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}ao untuk bantuan
+Ketik ${usedPrefix}hasa untuk bantuan
 Bonus: ${poin} XP
     `.trim()
     conn.asahotak[id] = [
-        await conn.sendBut(m.chat, caption, wm, 'Bantuan', '.ao', m),
+        await conn.sendButton(m.chat, caption, author, `${imgr + command}`, buttons, m),
         json, poin,
-        setTimeout(async () => {
-            if (conn.asahotak[id]) await conn.sendBut(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, wm, 'Asah Otak', '.asahotak', conn.asahotak[id][0])
+        setTimeout(() => {
+            if (conn.asahotak[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, author, null, [
+                ['asahotak', '/asahotak']
+            ], conn.asahotak[id][0])
             delete conn.asahotak[id]
         }, timeout)
     ]
@@ -31,3 +34,8 @@ handler.tags = ['game']
 handler.command = /^asahotak/i
 
 export default handler
+
+const buttons = [
+    ['Hint', '/hasa'],
+    ['Nyerah', 'menyerah']
+]
