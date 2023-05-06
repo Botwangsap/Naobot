acdf401854b60107e16d29da09b723cf89824172
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-05-07 06:43:21 +0700

    Update game-tekateki_hint.js

--- plugins/game-tekateki_hint.js
+++ plugins/game-tekateki_hint.js
@@ -1,12 +1,14 @@
 let handler = async (m, { conn }) => {
     conn.tekateki = conn.tekateki ? conn.tekateki : {}
     let id = m.chat
-    if (!(id in conn.tekateki)) throw 0
+    if (!(id in conn.tekateki)) throw false
     let json = conn.tekateki[id][1]
-    conn.reply(m.chat, '```' + json.bantuan + '```\nBalas soalnya, bukan pesan ini!', conn.tekateki[id][0])
+    conn.sendButton(m.chat, '```' + json.jawaban.replace(/[AIUEOaiueo]/ig, '_') + '```', author, null, [
+        ['Nyerah', 'menyerah']
+    ], m)
 }
-handler.command = /^tete$/i
+handler.command = /^htek$/i
 
-handler.limit = 1
+handler.limit = true
 
 export default handler
