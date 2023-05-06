5f271ac1b1ebc810817000d62edd25f260dfc148
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-05-06 23:59:16 +0700

    Update game-tebakgambar_answer.js

--- plugins/game-tebakgambar_answer.js
+++ plugins/game-tebakgambar_answer.js
@@ -2,37 +2,37 @@ import similarity from 'similarity'
 const threshold = 0.72
 export async function before(m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/Ketik.*hint/i.test(m.quoted.text) || /.*hint/i.test(m.text))
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/Ketik.*hgam/i.test(m.quoted.text) || /.*hgam/i.test(m.text))
         return !0
-    this.tebakgambar = this.tebakgambar ? this.tebakgambar : {}
-    if (!(id in this.tebakgambar))
-        return conn.sendButton(m.chat, 'Soal itu telah berakhir', author, null, buttonTebakgambar, m)
-    if (m.quoted.id == this.tebakgambar[id][0].id) {
+    this.tebakingambar = this.tebakingambar ? this.tebakingambar : {}
+    if (!(id in this.tebakingambar))
+        return conn.sendButton(m.chat, 'Soal itu telah berakhir', author, null, buttontebakingambar, m)
+    if (m.quoted.id == this.tebakingambar[id][0].id) {
         let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
         if (isSurrender) {
-            clearTimeout(this.tebakgambar[id][3])
-            delete this.tebakgambar[id]
-            return conn.sendButton(m.chat, '*Yah Menyerah :( !*', author, null, buttonTebakgambar, m)
+            clearTimeout(this.tebakingambar[id][3])
+            delete this.tebakingambar[id]
+            return conn.sendButton(m.chat, '*Yah Menyerah :( !*', author, null, buttontebakingambar, m)
         }
-        let json = JSON.parse(JSON.stringify(this.tebakgambar[id][1]))
+        let json = JSON.parse(JSON.stringify(this.tebakingambar[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.tebakgambar[id][2]
-            conn.sendButton(m.chat, `*Benar!*\n+${this.tebakgambar[id][2]} XP`, author, null, buttonTebakgambar, m)
-            clearTimeout(this.tebakgambar[id][3])
-            delete this.tebakgambar[id]
+            global.db.data.users[m.sender].exp += this.tebakingambar[id][2]
+            conn.sendButton(m.chat, `*Benar!*\n+${this.tebakingambar[id][2]} XP`, author, null, buttontebakingambar, m)
+            clearTimeout(this.tebakingambar[id][3])
+            delete this.tebakingambar[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold)
             m.reply(`*Dikit Lagi!*`)
         else
             conn.sendButton(m.chat, `*Salah!*`, author, null, [
-                ['hint', '/hint'],
-                ['nyerah', 'menyerah']
+                ['Hint', '/hgam'],
+                ['Nyerah', 'menyerah']
             ], m)
     }
     return !0
 }
 export const exp = 0
 
-const buttonTebakgambar = [
+const buttontebakingambar = [
     ['tebakgambar', '/tebakgambar']
 ]
