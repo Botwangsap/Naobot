e2aeda46e5f5d8e06bf501630b6e13ab82bbf8f9
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-05-07 06:40:39 +0700

    Update game-tekateki.js

--- plugins/game-tekateki.js
+++ plugins/game-tekateki.js
@@ -1,27 +1,28 @@
 import fetch from 'node-fetch'
 let timeout = 120000
-let poin = 500
-
-let handler = async (m, { conn, usedPrefix }) => {
+let poin = 4999
+let handler = async (m, { conn, command, usedPrefix }) => {
     conn.tekateki = conn.tekateki ? conn.tekateki : {}
     let id = m.chat
-    if (id in conn.tekateki) return conn.reply(m.chat, 'Belum dijawab!', conn.tekateki[id][0])
-    let res = await fetch(API('amel', '/tekateki', {}, 'apikey'))
-    if (!res.ok) throw eror
-    let json = await res.json()
-    if (!json.status) throw json
-    let caption = `
+    if (id in conn.tekateki) {
+        conn.sendButton(m.chat, 'Masih ada soal belum terjawab di chat ini', author, null, buttons, conn.tekateki[id][0])
+        throw false
+    }
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tekateki.json')).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `*${command.toUpperCase()}*
 ${json.soal}
-
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}tete untuk bantuan
+Ketik ${usedPrefix}htek untuk bantuan
 Bonus: ${poin} XP
-`.trim()
+    `.trim()
     conn.tekateki[id] = [
-        await conn.sendBut(m.chat, caption, wm, 'Bantuan', `.tete`, m),
+        await conn.sendButton(m.chat, caption, author, `https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=inferno-logo&doScale=false&scaleWidth=400&scaleHeight=400&fontsize=50&fillTextType=0&backgroundColor=black&text=${command}`, buttons, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tekateki[id]) conn.sendBut(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, wm, 'Teka Teki', `.tekateki`, conn.tekateki[id][0])
+            if (conn.tekateki[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, author, null, [
+                ['tekateki', '/tekateki']
+            ], conn.tekateki[id][0])
             delete conn.tekateki[id]
         }, timeout)
     ]
@@ -30,6 +31,9 @@ handler.help = ['tekateki']
 handler.tags = ['game']
 handler.command = /^tekateki/i
 
-handler.game = true
-
 export default handler
+
+const buttons = [
+    ['Hint', '/htek'],
+    ['Nyerah', 'menyerahtek']
+]
