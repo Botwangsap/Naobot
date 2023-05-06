0bc9691f24817221a98a141e4fa1c37cd9dbe43b
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-05-06 23:57:08 +0700

    Update game-tebakgambar.js

--- plugins/game-tebakgambar.js
+++ plugins/game-tebakgambar.js
@@ -1,29 +1,29 @@
 import { tebakgambar } from '@bochilteam/scraper'
 
-let timeout = 60000
+let timeout = 120000
 let poin = 4999
-let handler = async (m, { conn, usedPrefix }) => {
-    conn.tebakgambar = conn.tebakgambar ? conn.tebakgambar : {}
+let handler = async (m, { conn, command, usedPrefix }) => {
+    conn.tebakingambar = conn.tebakingambar ? conn.tebakingambar : {}
     let id = m.chat
-    if (id in conn.tebakgambar) {
-        conn.sendButton(m.chat, 'Masih ada soal belum terjawab di chat ini', author, null, buttons, conn.tebakgambar[id][0])
+    if (id in conn.tebakingambar) {
+        conn.sendButton(m.chat, 'Masih ada soal belum terjawab di chat ini', author, null, buttons, conn.tebakingambar[id][0])
         throw false
     }
     let json = await tebakgambar()
-    // if (!json.status) throw json
-    let caption = `
+    let caption = `*${command.toUpperCase()}*
+Rangkailah Gambar Ini
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}hint untuk hint
+Ketik ${usedPrefix}hgam untuk bantuan
 Bonus: ${poin} XP
     `.trim()
-    conn.tebakgambar[id] = [
+    conn.tebakingambar[id] = [
         await conn.sendButton(m.chat, caption, author, json.img, buttons, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakgambar[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, author, null, [
+            if (conn.tebakingambar[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, author, null, [
                 ['tebakgambar', '/tebakgambar']
-            ], conn.tebakgambar[id][0])
-            delete conn.tebakgambar[id]
+            ], conn.tebakingambar[id][0])
+            delete conn.tebakingambar[id]
         }, timeout)
     ]
 }
@@ -34,6 +34,6 @@ handler.command = /^tebakgambar/i
 export default handler
 
 const buttons = [
-    ['hint', '/hint'],
-    ['nyerah', 'menyerah']
+    ['Hint', '/hgam'],
+    ['Nyerah', 'menyerah']
 ]
